9c2f685000ed1ab41e68aa5093f7c0ea25938267
Waradu <waradu@outlook.com>
2023-11-16 08:37:58 +0100

    feat: Add script injection functionality
    This commit adds functionality to the service worker to inject a script
    file and CSS file into the active tab upon receiving the "inject-script"
    command. This allows for dynamic modification of the page content.
    
    - Update service_worker.js to include the script and CSS injection logic
    - Update manifest.json to include the "inject-script" command with a
    suggested keybinding of "Alt+S" and a description of "Inject in page"

--- Eggcellent_Ext/data/background/service_worker.js
+++ Eggcellent_Ext/data/background/service_worker.js
@@ -4,4 +4,20 @@ chrome.commands.onCommand.addListener(async (command) => {
       url: "../data/pages/main/index.html"
     });
   }
+  if (command === "inject-script") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      // get the current tab ID
+      let tabId = tabs[0].id;
+      // inject the script file
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["../data/pages/main/script.js"]
+      });
+      // inject the CSS file
+      chrome.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: ["../data/pages/main/style.css"]
+      });
+    });
+  }
 });
