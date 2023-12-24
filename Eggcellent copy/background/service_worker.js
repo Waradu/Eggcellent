a4bd8758b46cf5db96b582b20ed09146c0f34704
Waradu <waradu@outlook.com>
2023-12-24 15:28:28 +0100

    update

---
+++ Eggcellent copy/background/service_worker.js
@@ -0,0 +1,23 @@
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command == "open_search_page") {
+    var tabs = await chrome.tabs.query({ currentWindow: true });
+
+    tabs.forEach(function (tab) {
+      if (tab.url == "chrome://newtab/" || tab.url == "edge://newtab/") {
+        chrome.tabs.remove(tab.id);
+      }
+    });
+
+    var tabWithUrl = tabs.filter(function (tab) {
+      return tab.url.endsWith("/data/pages/main/index.html");
+    });
+
+    if (tabWithUrl.length > 0) {
+      chrome.tabs.update(tabWithUrl[0].id, { active: true });
+    } else {
+      chrome.tabs.create({
+        url: "../data/pages/main/index.html",
+      });
+    }
+  }
+});
