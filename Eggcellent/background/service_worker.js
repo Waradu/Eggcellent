b48126ebed4d2786a5105b0bb6502a5f0965d88e
Waradu <waradu@outlook.com>
2023-12-28 10:44:12 +0100

    - fixed fav icon - fixed browser close on alt + s

--- Eggcellent/background/service_worker.js
+++ Eggcellent/background/service_worker.js
@@ -2,14 +2,10 @@ chrome.commands.onCommand.addListener(async (command) => {
   if (command == "open_search_page") {
     var tabs = await chrome.tabs.query({ currentWindow: true });
 
-    tabs.forEach(function (tab) {
-      if (tab.url == "chrome://newtab/" || tab.url == "edge://newtab/") {
-        chrome.tabs.remove(tab.id);
-      }
-    });
-
     var tabWithUrl = tabs.filter(function (tab) {
-      return tab.url.endsWith("/pages/main/index.html");
+      var url = new URL(tab.url);
+      url.search = "";
+      return url.toString().endsWith("/pages/main/index.html");
     });
 
     if (tabWithUrl.length > 0) {
@@ -19,6 +15,12 @@ chrome.commands.onCommand.addListener(async (command) => {
         url: "../pages/main/index.html",
       });
     }
+
+    tabs.forEach(function (tab) {
+      if (tab.url == "chrome://newtab/" || tab.url == "edge://newtab/") {
+        chrome.tabs.remove(tab.id);
+      }
+    });
   }
 });
 
@@ -26,6 +28,6 @@ chrome.runtime.onInstalled.addListener(function (object) {
   let internalUrl = chrome.runtime.getURL("pages/main/index.html?help=true");
 
   if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-      chrome.tabs.create({ url: internalUrl });
+    chrome.tabs.create({ url: internalUrl });
   }
 });
