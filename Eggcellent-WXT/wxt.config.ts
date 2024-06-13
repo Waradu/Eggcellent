b046373ef17faf4ec86ea2721b926cbcb9eaca7f
Waradu <waradu@outlook.com>
2024-06-13 17:11:55 +0200

    test vue

---
+++ Eggcellent-WXT/wxt.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'wxt';
+import vue from '@vitejs/plugin-vue';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  imports: {
+    addons: {
+      vueTemplate: true,
+    },
+  },
+  manifest: {
+    
+  },
+  vite: () => ({
+    plugins: [vue()],
+    build: {
+      // Enabling sourcemaps with Vue during development is known to cause problems with Vue
+      sourcemap: false,
+    },
+  }),
+});
